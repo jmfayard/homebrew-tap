08d4b72bfc4aac29740550e6199e424aaa6f3e4a
Holger Brandl <holgerbrandl@gmail.com>
2017-11-07 11:32:52 +0100

    v2.1.1 release

---
+++ kscript.rb
@@ -0,0 +1,14 @@
+class Kscript < Formula
+  desc "kscript"
+  homepage "https://github.com/holgerbrandl/kscript"
+  url "https://github.com/holgerbrandl/kscript/releases/download/v2.1.1/kscript-2.1.1-bin.zip"
+  sha256 "415dd1e3cbee0e2ab223449f5b76a013"
+
+  depends_on "kotlin"
+
+  def install
+    libexec.install Dir["*"]
+    inreplace "#{libexec}/bin/kscript", /^jarPath=.*/, "jarPath=#{libexec}/bin/kscript.jar"
+    bin.install_symlink "#{libexec}/bin/kscript"
+  end
+end
