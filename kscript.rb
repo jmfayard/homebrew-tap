936ff69ac6bb6b7c96f58b6c3fb5fc2c6cadbb6e
Holger Brandl <holger.brandl@systemagmbh.de>
2019-02-16 15:01:39 +0100

    v2.7.0 release

--- kscript.rb
+++ kscript.rb
@@ -1,8 +1,8 @@
 class Kscript < Formula
   desc "kscript"
   homepage "https://github.com/holgerbrandl/kscript"
-  url "https://github.com/holgerbrandl/kscript/releases/download/v2.6.0/kscript-2.6.0-bin.zip"
-  sha256 "7812c2f2d2ec2cb94262024559a3214a3857ba1f04750bea17d534a3d3bc27c7"
+  url "https://github.com/holgerbrandl/kscript/releases/download/v2.7.0/kscript-2.7.0-bin.zip"
+  sha256 "f132034bbb7a756be72ca21a3677552560f43bffdaf0e3e2ce052477a799bb0d"
 
   depends_on "kotlin"
 
