74d20660f3a559a3376d5d5426f5f1e2dbc9f427
holgerbrandl@gmail.com <holgerbrandl@users.noreply.github.com>
2020-01-17 07:53:37 +0100

    v2.9.3 release

--- kscript.rb
+++ kscript.rb
@@ -1,8 +1,8 @@
 class Kscript < Formula
   desc "kscript"
   homepage "https://github.com/holgerbrandl/kscript"
-  url "https://github.com/holgerbrandl/kscript/releases/download/v2.9.0/kscript-2.9.0-bin.zip"
-  sha256 "85bbed4e20d40613b06c0e11cc9358b36000afebeb613baf07e975448ea18f2c"
+  url "https://github.com/holgerbrandl/kscript/releases/download/v2.9.3/kscript-2.9.3-bin.zip"
+  sha256 "d15f4236abb9341c330862fea0fdcdba7818b63953fdb3885720bbc1901666d1"
 
   depends_on "kotlin"
 
